cd8e5d9f42274b10ea59988489943c03af0db4de
plucky <plucky.pan@ubnt.com>
2017-06-28 00:21:50 +0800

    remove useless file

--- process/render/index.js
+++ process/render/index.js
@@ -1,5 +1,9 @@
-const ipc = require('electron').ipcRenderer;
 $(function() {
+	const ipc = require('electron').ipcRenderer;
+	ipc.on('ASYNC-MSG-Replay', function(event, arg) {
+		$("#preResult").html(arg);
+	})
+
 	$("#btnSync").click(function() {
 		const reply = ipc.sendSync('SYNC-MSG', 'ping');
 		$("#preResult").html(reply);
@@ -20,7 +24,45 @@ $(function() {
 			}
 		});
 	});
+
+	$("#li-dashboard").click(function() {
+		$("#li-dashboard").attr("class", "active");
+		$("#li-about").attr("class", "");
+		$("#col-dashboard").css("display", "block");
+		$("#col-about").css("display", "none");
+	});
+	$("#li-about").click(function() {
+		renderMap();
+		$("#li-dashboard").attr("class", "");
+		$("#li-about").attr("class", "active");
+		$("#col-dashboard").css("display", "none");
+		$("#col-about").css("display", "block");
+	});
 });
-ipc.on('ASYNC-MSG-Replay', function(event, arg) {
-	$("#preResult").html(arg);
-})
+
+function renderMap() {
+	var mapData = {
+		"CN": 100
+	};
+	$('#world-map').html("");
+	$('#world-map').vectorMap({
+		map: 'world_mill_en',
+		backgroundColor: "transparent",
+		regionStyle: {
+			initial: {
+				fill: '#e4e4e4',
+				"fill-opacity": 1,
+				stroke: 'none',
+				"stroke-width": 0,
+				"stroke-opacity": 0
+			}
+		},
+		series: {
+			regions: [{
+				values: mapData,
+				scale: ["#1ab394", "#22d6b1"],
+				normalizeFunction: 'polynomial'
+			}]
+		}
+	});
+}
