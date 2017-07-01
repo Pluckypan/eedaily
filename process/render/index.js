fa43f937d61983f7b4f9ec182aaf9b0b9c08e8a4
plucky <plucky.pan@ubnt.com>
2017-07-01 15:18:36 +0800

    remove map

--- process/render/index.js
+++ process/render/index.js
@@ -32,37 +32,9 @@ $(function() {
 		$("#col-about").css("display", "none");
 	});
 	$("#li-about").click(function() {
-		renderMap();
 		$("#li-dashboard").attr("class", "");
 		$("#li-about").attr("class", "active");
 		$("#col-dashboard").css("display", "none");
 		$("#col-about").css("display", "block");
 	});
 });
-
-function renderMap() {
-	var mapData = {
-		"CN": 100
-	};
-	$('#world-map').html("");
-	$('#world-map').vectorMap({
-		map: 'world_mill_en',
-		backgroundColor: "transparent",
-		regionStyle: {
-			initial: {
-				fill: '#e4e4e4',
-				"fill-opacity": 1,
-				stroke: 'none',
-				"stroke-width": 0,
-				"stroke-opacity": 0
-			}
-		},
-		series: {
-			regions: [{
-				values: mapData,
-				scale: ["#1ab394", "#22d6b1"],
-				normalizeFunction: 'polynomial'
-			}]
-		}
-	});
-}
