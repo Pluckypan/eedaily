563af2b32cdf1a4a8febc40b65cbdf5738ced3f7
plucky <plucky.pan@ubnt.com>
2017-07-02 16:14:03 +0800

    add electron api

--- process/main/ipcMain.js
+++ process/main/ipcMain.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const ipc = electron.ipcMain;
+const dialog = electron.dialog;
 const BrowserWindow = electron.BrowserWindow;
 var rootPath = require('path').resolve(__dirname, "../../");
 ipc.on('SYNC-MSG', function(event, arg) {
@@ -7,4 +8,11 @@ ipc.on('SYNC-MSG', function(event, arg) {
 })
 ipc.on('ASYNC-MSG', function(event, arg) {
 	event.sender.send('ASYNC-MSG-Replay', arg);
+})
+ipc.on('open-file-dialog', function (event) {
+  dialog.showOpenDialog({
+    properties: ['openFile', 'openDirectory']
+  }, function (files) {
+    if (files) event.sender.send('selected-directory', files)
+  })
 })
--- process/render/index.js
+++ process/render/index.js
@@ -1,7 +1,16 @@
 $(function() {
 	const ipc = require('electron').ipcRenderer;
+	const shell = require('electron').shell;
+	const BrowserWindow = require('electron').remote.BrowserWindow;
+	const os = require('os');
+	var path = require('path');
+
 	ipc.on('ASYNC-MSG-Replay', function(event, arg) {
 		$("#preResult").html(arg);
+	})
+
+	ipc.on('selected-directory', function(event, path) {
+		$("#preResult").html(`You selected: ${path}`);
 	})
 
 	$("#btnSync").click(function() {
@@ -24,6 +33,40 @@ $(function() {
 			}
 		});
 	});
+	$("#btnNotify").click(function() {
+		const notification = {
+			title: '下载完成',
+			body: 'xxx-20170630-ja.avi'
+		};
+		const mNotification = new window.Notification(notification.title, notification)
+		mNotification.onclick = () => {
+			$("#preResult").html('Notification clicked');
+		}
+	});
+	$("#btnOpenPath").click(function() {
+		shell.showItemInFolder(os.homedir());
+	});
+	$("#btnOpenUrl").click(function() {
+		shell.openExternal('http://www.echo.engineer')
+	});
+	$("#btnOpenFileAndDir").click(function() {
+		ipc.send('open-file-dialog');
+	});
+	$("#btnEditor").click(function() {
+		const modalPath = path.join("file://", __dirname, "editor.html");
+		$("#preResult").html(modalPath);
+		let win = new BrowserWindow({
+			width: 1024,
+			height: 768,
+			minWidth: 1000,
+			minHeight: 600
+		});
+		win.on('close', function() {
+			win = null
+		});
+		win.loadURL(modalPath);
+		win.show();
+	});
 
 	$("#li-dashboard").click(function() {
 		$("#li-dashboard").attr("class", "active");
@@ -36,5 +79,139 @@ $(function() {
 		$("#li-about").attr("class", "active");
 		$("#col-dashboard").css("display", "none");
 		$("#col-about").css("display", "block");
+	});
+
+	var configPath = path.resolve(__dirname, "../config.json");
+	var jsPath = path.resolve(__dirname, "lib/eeutils.js");
+	var eeutils = require(jsPath);
+	if(eeutils.exists(configPath)) {
+		var configStr = eeutils.read(configPath);
+		$("#preResult").html(configStr + "");
+	} else {
+		var cfg = {
+			"A": "A"
+		};
+		var cfgStr = JSON.stringify(cfg);
+		eeutils.write(configPath, cfgStr);
+		$("#preResult").html(cfgStr);
+	}
+
+	$('#using_json').jstree({
+		'plugins': ['types', 'dnd'],
+		'types': {
+			'default': {
+				'icon': 'fa fa-folder'
+			},
+			'html': {
+				'icon': 'fa fa-file-code-o'
+			},
+			'svg': {
+				'icon': 'fa fa-file-picture-o'
+			},
+			'css': {
+				'icon': 'fa fa-file-code-o'
+			},
+			'img': {
+				'icon': 'fa fa-file-image-o'
+			},
+			'js': {
+				'icon': 'fa fa-file-text-o'
+			}
+
+		},
+		'core': {
+			'data': [
+				'Empty Folder',
+				{
+					'text': 'Resources',
+					'state': {
+						'opened': true
+					},
+					'children': [{
+							'text': 'css',
+							'children': [{
+									'text': 'animate.css',
+									'type': 'css'
+
+								},
+								{
+									'text': 'bootstrap.css',
+									'type': 'css'
+								},
+								{
+									'text': 'main.css',
+									'type': 'css'
+								},
+								{
+									'text': 'style.css',
+									'type': 'css'
+								}
+							],
+							'state': {
+								'opened': true
+							}
+						},
+						{
+							'text': 'js',
+							'children': [{
+									'text': 'bootstrap.js',
+									'type': 'js'
+								},
+								{
+									'text': 'inspinia.min.js',
+									'type': 'js'
+								},
+								{
+									'text': 'jquery.min.js',
+									'type': 'js'
+								},
+								{
+									'text': 'jsTree.min.js',
+									'type': 'js'
+								},
+								{
+									'text': 'custom.min.js',
+									'type': 'js'
+								}
+							],
+							'state': {
+								'opened': true
+							}
+						},
+						{
+							'text': 'html',
+							'children': [{
+									'text': 'layout.html',
+									'type': 'html'
+								},
+								{
+									'text': 'navigation.html',
+									'type': 'html'
+								},
+								{
+									'text': 'navbar.html',
+									'type': 'html'
+								},
+								{
+									'text': 'footer.html',
+									'type': 'html'
+								},
+								{
+									'text': 'sidebar.html',
+									'type': 'html'
+								}
+							],
+							'state': {
+								'opened': true
+							}
+						}
+					]
+				}
+			]
+		}
+	});
+	$('#using_json').on("changed.jstree", function(e, data) {
+		$("#preResult").html("ID=" + data.node.id + " test=" + data.node.text);
+
 	});
 });
